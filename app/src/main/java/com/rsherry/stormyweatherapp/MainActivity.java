d05f51581d80680c539ad1ce7e794db1fe0eb651
Ryan Sherry <rsherrycomputers@gmail.com>
2018-05-19 23:21:00 -0700

    final changes

--- app/src/main/java/com/rsherry/stormyweatherapp/MainActivity.java
+++ app/src/main/java/com/rsherry/stormyweatherapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.rsherry.stormyweatherapp;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AlertDialog;
@@ -9,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,9 +35,19 @@ public class MainActivity extends AppCompatActivity {
 
     private CurrentWeather currentWeather;
 
+    private ImageView iconImage;
+
+    final double latitude = 37.8267;
+    final double longitude = -122.4233;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getForecast(latitude,longitude);
+            Log.d(TAG, "Main UI code is running, horray!");
+    }
+
+    private void getForecast(double latitude, double longitude) {
         final ActivityMainBinding binding = DataBindingUtil.setContentView(MainActivity.this,
                 R.layout.activity_main);
 
@@ -42,9 +55,10 @@ public class MainActivity extends AppCompatActivity {
 
         darkSky.setMovementMethod(LinkMovementMethod.getInstance());
 
+        iconImage = findViewById(R.id.iconImageView);
+
         String apiKey = ApiKey.getApiKey();
-        double latitude = 37.8267;
-        double longitude = -122.4233;
+
         String forecastURL = "https://api.darksky.net/forecast/"
                 + apiKey + "/" + latitude + "," + longitude;
 
@@ -72,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
                             currentWeather = getCurrentDetails(jsonData);
 
-                            CurrentWeather displayWeather = new CurrentWeather(
+                            final CurrentWeather displayWeather = new CurrentWeather(
                                     "Alcatrez Island, CA",
                                     currentWeather.getIcon(),
                                     currentWeather.getTime(),
@@ -84,6 +98,15 @@ public class MainActivity extends AppCompatActivity {
                             );
 
                             binding.setWeather(displayWeather);
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Drawable drawable = getResources().getDrawable(displayWeather.getIconId());
+                                    iconImage.setImageDrawable(drawable);
+                                }
+                            });
+
 
                         } else {
                             alertUserOfError();
@@ -98,7 +121,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             alertUserOfNoNetwork();
         }
-            Log.d(TAG, "Main UI code is running, horray!");
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
@@ -155,6 +177,11 @@ public class MainActivity extends AppCompatActivity {
         AlertDialogueFragment dialogue = new AlertDialogueFragment();
         dialogue.setArguments(bundle);
         dialogue.show(getFragmentManager(),"network_error_dialogue");
+    }
+
+    public void refreshOnClick(View view) {
+        getForecast(latitude,longitude);
+        Toast.makeText(this,"Refreshing Data",Toast.LENGTH_LONG).show();
     }
 }
 
